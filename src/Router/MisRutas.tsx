86726e5f0d5a9467315263c3496ad6396bdddecd
RodrigoDuvivier <rodrigo.martinez224@gmail.com>
2025-07-03 00:38:52 -0300

    v2

--- src/Router/MisRutas.tsx
+++ src/Router/MisRutas.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Footer } from '../components/Footer';
 import { Contacto } from '../components/Contacto';
--- src/components/inicio.tsx
+++ src/components/inicio.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
 import { HeroSection } from './Layout/HeroSection'
 
 export const Inicio = () => {
   return (
     <div>
 
-      <h1>INICIO</h1>
+      
 
       <HeroSection />
 
-
+      <div className="p-8 text-center">
+        <h1 className="text-4xl font-bold">Bienvenido a GlobalOffice</h1>
+        <p className="mt-2 text-lg">Tu solución integral para el manejo de inventarios.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, perferendis omnis ratione esse neque eos earum labore accusantium ipsum expedita sunt reiciendis laborum odio beatae saepe tempore. Delectus, quo laborum. </p>
+      </div>
     </div>
   )
 }
